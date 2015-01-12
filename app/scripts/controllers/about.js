c49bad194cf7952fcdf9939abe94efa2605eb51f
Bruno Cau <brunoalcau@gmail.com>
2015-01-12 16:08:17 +0000

    Criaçao da aplicaçao

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bibliotecasSystemApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bibliotecasSystemApp
+ */
+angular.module('bibliotecasSystemApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bibliotecasSystemApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bibliotecasSystemApp
+ */
+angular.module('bibliotecasSystemApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
