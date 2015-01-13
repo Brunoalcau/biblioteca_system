797fe53260f3ec1f955d64ab8488dc5330c2f85e
Bruno Alves <BrunoCau@MacBook-Pro-de-Bruno.local>
2015-01-13 01:17:22 -0300

    colocar no karma injecao de dependencia

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -26,6 +26,8 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular-formstamp/formstamp.js',
+      'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
